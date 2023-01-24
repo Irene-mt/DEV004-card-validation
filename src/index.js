6e9de51baac4d3571a80abee0131d9aefa0d8568
Irene Martínez <marttre20@gmail.com>
2023-01-24 11:43:27 -0600

    Versión 1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
-import validator from './validator.js';
+import validator from "./validator.js";
+const botonValidar = document.getElementById("botonValid");
+const resultado = document.getElementById("resultado");
+const ingresar = document.querySelector(".ingresar");
+const mostrar = document.querySelector(".mostrar");
+botonValidar.addEventListener("click", () => {
+  const card = document.getElementById("cardNumber").value;
 
-console.log(validator);
+  if (card.length === 0) {
+    return alert("Campo vacío. Por favor ingresa un número.");
+  }
+
+  if (card.length > 18 || card.length < 13) {
+    return alert("Ingresa de 13 a 18 dígitos.");
+  }
+  ingresar.style.display = "none";
+  const enmascarar = validator.maskify(card);
+  mostrar.style.display = "block";
+
+  const valid = validator.isValid(card);
+  if (valid) {
+    resultado.innerHTML = `Tu tarjeta ${enmascarar} es válida`;
+  } else {
+    resultado.innerHTML = `Tu tarjeta ${enmascarar} es inválida`;
+  }
+});
+//console.log(validator);
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,44 @@
 const validator = {
-  // ...
+  isValid(creditCardNumber) {
+    let reverseNumber;
+    for (let i = 1; i <= 9; i++) {
+      const reversa = creditCardNumber.split("").reverse();
+      reverseNumber = reversa;
+    }
+    const arrayMultiplicar = [];
+    for (let j = 0; j < reverseNumber.length; j++) {
+      if (j % 2 === 1) {
+        const multiplicar = parseInt(reverseNumber[j] * 2); //parse
+        if (multiplicar > 9) {
+          const numDoble = multiplicar;
+          const num1 = numDoble.toString().charAt(0);
+          const parte1 = parseInt(num1);
+          const num2 = numDoble.toString().charAt(1);
+          const parte2 = parseInt(num2);
+          const suma = parte1 + parte2;
+          arrayMultiplicar.push(suma);
+        } else {
+          arrayMultiplicar.push(multiplicar);
+        }
+      } else if (j % 2 === 0) {
+        const noMultiplicar = parseInt(reverseNumber[j]); //parse
+        arrayMultiplicar.push(noMultiplicar);
+      }
+    }
+    const total = function (n1, n2) {
+      return n1 + n2;
+    };
+    const resultado = arrayMultiplicar.reduce(total);
+    //console.log("La suma da " + arrayMultiplicar.reduce(total));
+    if (resultado % 10 === 0) {
+      return true;
+    } else {
+      return false;
+    }
+  },
+  maskify(creditCardNumber) {
+    return creditCardNumber.replace(/.(?=.{4})/g, "#");
+  }
 };
 
 export default validator;
